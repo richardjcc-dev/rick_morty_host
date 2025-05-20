6aa5a116350abc1897e782b98407db2b49e29dfc
Richard Cedeno <r.cedeno@logderin.com>
2025-05-20 07:06:32 -0400

    Arreglos al layout del Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
 import CharacterSearcher from 'rick_morty_remote/CharacterSearcher'
 import CharacterCard from 'rick_morty_remote/CharacterCard'
+import CharacterDetails from 'rick_morty_remote/CharacterDetails'
+import CharactersFilters from 'rick_morty_remote/CharactersFilters'
 
 function HomePage() {
+  const [show, setShow] = useState(false)
+  const [activeTab, setActiveTab] = useState('all')
+  // const [selectedCharacter, setSelectedCharacter] = useState({})
+
+  const handleCloseDetails = () => setShow(false)
+  const handleShowDetails = () => setShow(true)
+
+  const character = {
+    name: 'Rick Sanchez',
+    species: 'Human',
+    gender: 'Male',
+    status: 'Alive',
+    location: 'Citadel of Ricks',
+    origin: 'Pilot',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  }
+
   return (
     <section className="main-page-container">
       <header className="header py-2">
@@ -17,8 +37,84 @@ function HomePage() {
       </header>
 
       <section className="content">
-        <CharacterCard />
-        <CharacterCard />
+        <div className="d-flex justify-content-between align-items-center w-100 mt-1 mb-5">
+          <div className="home-breadcrumb px-3">
+            <button
+              className={`btn ${activeTab === 'all' ? 'btn-success' : ''}`}
+              onClick={() => setActiveTab('all')}
+            >
+              All
+            </button>
+            <button
+              className={`btn breadcrumb-btn ${activeTab === 'favorites' ? 'btn-success' : ''}`}
+              onClick={() => setActiveTab('favorites')}
+            >
+              Favorites
+            </button>
+          </div>
+          <div>
+            <CharactersFilters />
+          </div>
+        </div>
+        {activeTab === 'all' && (
+          <div className="content mt-0">
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+            <div className="mb-3">
+              <CharacterCard
+                onClick={handleShowDetails}
+                character={character}
+              />
+            </div>
+
+            <CharacterDetails
+              character={character}
+              onHide={handleCloseDetails}
+              show={show}
+            />
+          </div>
+        )}
+        {activeTab === 'favorites' && <div>Favoritos</div>}
       </section>
       <footer className="footer">
         <p>TM & © 2024 The Cartoon Network, Inc. All Rights Reserved.</p>
--- src/tsremote_declarations.d.ts
+++ src/tsremote_declarations.d.ts
@@ -1,2 +1,4 @@
 declare module 'rick_morty_remote/CharacterCard'
 declare module 'rick_morty_remote/CharacterSearcher'
+declare module 'rick_morty_remote/CharactersFilters'
+declare module 'rick_morty_remote/CharacterDetails'
